bdf23a20db9e5ae38cf7af46b43da68ffa43ff73
Michał Błajet <michal.blajet00@gmail.com>
2023-03-22 23:31:20 +0100

    RSWW-10: add simple db connection for the command service

---
+++ accommodation_service_commands/src/main/java/pl/edu/pg/accommodation/ping/PingEntity.java
@@ -0,0 +1,52 @@
+package pl.edu.pg.accommodation.ping;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class PingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "date")
+    private LocalDateTime pingDateTime;
+
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getPingDateTime() {
+        return pingDateTime;
+    }
+
+    public void setPingDateTime(final LocalDateTime time) {
+        this.pingDateTime = time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "PingEntity{" +
+                "id=" + id +
+                ", pingDateTime=" + pingDateTime +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
--- accommodation_service_commands/src/main/java/pl/edu/pg/accommodation/ping/PingEventListener.java
+++ accommodation_service_commands/src/main/java/pl/edu/pg/accommodation/ping/PingEventListener.java
@@ -3,13 +3,29 @@ package pl.edu.pg.accommodation.ping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class PingEventListener {
     private final static Logger logger = LoggerFactory.getLogger(PingEventListener.class);
+
+    private final PingRepository repository;
+
+    @Autowired
+    public PingEventListener(final PingRepository repository) {
+        this.repository = repository;
+    }
+
     @RabbitListener(queues = "${spring.rabbitmq.ping.queue}")
     public void receiveMessage(String message) {
         logger.info("I'm here!!! Your message: {}", message);
+        final var entity = new PingEntity();
+        entity.setPingDateTime(LocalDateTime.now());
+        entity.setMessage(message);
+        repository.save(entity);
+        logger.info("Pings so far: {}", repository.findAll());
     }
 }
