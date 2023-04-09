b9baa8c18b4f9908111f97b1fcf65954879b2b1a
Winetq <winetq9679@gmail.com>
2023-04-10 00:29:06 +0200

    RSWW-21 Create initial transport service for commands

---
+++ transport_service_commands/src/main/java/pl/edu/pg/config/RabbitMqConfig.java
@@ -0,0 +1,35 @@
+package pl.edu.pg.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class RabbitMqConfig {
+
+    @Value("${spring.rabbitmq.queue}")
+    private String queue;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        return cachingConnectionFactory;
+    }
+}
