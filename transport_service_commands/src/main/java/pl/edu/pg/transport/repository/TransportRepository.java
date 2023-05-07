263b9f642cb7088e72c3a88b75cbe67fe556bb5e
Winetq <winetq9679@gmail.com>
2023-05-07 16:57:13 +0200

    Pull changes from master

--- transport_service_commands/src/main/java/pl/edu/pg/transport/repository/TransportRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pg.transport.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.pg.transport.entity.Transport;
-
-@Repository
-public interface TransportRepository extends JpaRepository<Transport, Long> {
-}
--- transport_service_queries/src/main/java/pl/edu/pg/transport/event/GenericEvent.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pg.transport.event;
-
-public class GenericEvent {
-    private String source;
-
-    public String getSource() {
-        return source;
-    }
-}
